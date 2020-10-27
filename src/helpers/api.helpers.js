759ae0f8ce20da0e14c5c20c42086ac1bc9d0504
Dmitry Kryukov <remasik@gmail.com>
2020-10-26 20:01:13 -0700

    Update

--- src/helpers/api.helpers.js
+++ src/helpers/api.helpers.js
@@ -5,7 +5,7 @@ import Constants from "./constants";
 export default axios.create({
   baseURL: Constants.API.url,
   headers: {
-    "Accept": "application/json",
+    "Accept": "application/vnd.github.v3+json",
     "Content-Type": "application/json",
     "If-None-Match": "",
     "Authorization": `token ${store.getState().settingsReducer.token}`,
